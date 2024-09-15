87cbaa3dd6d658e37d5c414ac1247753f81e9dbe
chntlg <chantalguerrero04@gmail.com>
2024-09-15 15:22:41 +0800

    Initial commit with dataConverdions.js

---
+++ dataConversion.js
@@ -0,0 +1,27 @@
+//Convert string to number
+//parseInt()
+let price = '200';
+price = parseInt(price);
+console.log(price);
+// + 
+price = + (price);
+console.log(price);
+//Number()
+price = Number(price)
+console.log(price);
+//console.log(typeof price);
+
+//Convert number to string
+let distance = 150;
+distance = distance.toString();
+console.log(distance);
+//String()
+distance = String(distance) ;
+console.log(distance);
+//console.log(typeof distance)
+
+//Convert string to decimal
+let weight = "72.5";
+weight = parseFloat(weight);
+console.log(weight)
+//console.log(typeof weight)
